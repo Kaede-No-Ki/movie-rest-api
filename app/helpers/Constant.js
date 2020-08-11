bf86fdf43f289eb162c58cdc9031bcb276b83474
rifkiystark <ananda.rifkiy32@gmail.com>
2020-08-11 20:10:21 +0700

    add production server

--- app/helpers/Constant.js
+++ app/helpers/Constant.js
@@ -6,5 +6,8 @@ module.exports = {
   westUrl: "serial-barat/",
   asianUrl: "drama-asia/",
   moviesUrl: "movies/",
-  urlApi: `${process.env.URL_API_DEV}:${process.env.PORT}/`,
+  urlApi:
+    process.env.MODE == "DEVELOPMENT"
+      ? `${process.env.URL_API_DEV}:${process.env.PORT}/`
+      : process.env.URL_API_PROD,
 };
